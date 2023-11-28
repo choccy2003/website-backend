c4921e941631c98b79f9f37cc279e49951eff0f0
choccy2 <kujoxd6@gmail.com>
2023-11-28 21:06:38 +0530

    1.0

--- app.js
+++ app.js
@@ -67,5 +67,9 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+process.on('SIGINT', () => {
+  console.log('Closing MongoDB connection');
+  client.close();
+  process.exit();
+});
 module.exports = app;
